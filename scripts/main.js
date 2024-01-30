0491b4e45edf1e42d1e547b89874a94217bc1832
Grégoire Nieto <gregoire.nieto@zaclys.net>
2024-01-30 17:07:58 +0100

    exo récupérer éléments du DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-lancerJeu()
+let zoneInputJoueur = document.getElementById("inputEcriture")
+let zoneBoutonValidation = document.getElementById("btnValiderMot")
+
+let zoneMotPropose = document.querySelector(".zoneProposition")
+let zoneScore = document.querySelector(".zoneScore span")
+
+let listeBoutons = document.querySelectorAll(".optionSource input")
+
+//lancerJeu()
